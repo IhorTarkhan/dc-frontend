04b17cd862058778c01048517bfd1ed83bbb12c7
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-06-11 17:30:16 +0300

    spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import image from "./img/image.svg";
 import "./App.css";
+import { Spinner } from "./Spinner";
 
 export const App = () => {
   return (
-    <div className={"App"}>
-      <header className={"App-header"}>
-        <img src={image} className={"App-logo"} alt={"image"} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={"App-link"}
-          href={"https://reactjs.org"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Spinner />
+    </>
   );
 };

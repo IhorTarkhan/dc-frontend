284f628c302557e96f20476da34f73ce68983d54
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-06-13 11:32:12 +0300

    new album template added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Header } from "./header/Header";
 import { Line1 } from "./divider/Line1";
 import { Line2 } from "./divider/Line2";
 import { Footer } from "./footer/Footer";
+import { NewAlbum } from "./new-album/NewAlbum";
 
 export const App = () => {
   return (
     <>
       {/*<Spinner />*/}
       <Header />
+      <NewAlbum />
       <Line1 />
       <Line2 />
       <Footer />
---
+++ src/new-album/NewAlbum.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./NewAlbum.css";
+import { SocialNetworks } from "../social/SocialNetworks";
+import { MusicServices } from "../social/MusicServices";
+
+export const NewAlbum = () => {
+  return (
+    <div className="new-album">
+      <div className="new-album__wrapper">
+        <img
+          className="new-album__image image"
+          loading="lazy"
+          src="assets/img/x2/darwins-cat.png"
+          alt="New Album"
+        />
+      </div>
+      <div className="new-album__wrapper">
+        <MusicServices />
+        <h2 className="new-album__title title">2020 Album</h2>
+        <p className="new-album__text text">
+          Our first album contains stories about who is Darwin's Cat, how Cat's
+          Civilisation borns, a story about Cat's evolution process, long story
+          about what Darwin's Cat saw when he returned to the cat's civilisation
+          mother planet, and how Cats got Wings. And, of course, what usually
+          cyber cats do and how they interact with Humans. Verbal terms of the
+          tales are adapted to be understandable for Humans.
+        </p>
+        <a className="new-album__button button" href="#music.html">
+          Read more
+        </a>
+
+        <SocialNetworks />
+      </div>
+    </div>
+  );
+};

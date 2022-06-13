ba12e46a57811c3ebd40c6dfa8236bd1cc60d7e3
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-06-13 15:51:52 +0300

    new-album correct css

--- src/new-album/NewAlbum.tsx
+++ src/new-album/NewAlbum.tsx
@@ -6,19 +6,14 @@ import logo from "../img/darwins-cat.png";
 
 export const NewAlbum = () => {
   return (
-    <div className="new-album">
-      <div className="new-album__wrapper">
-        <img
-          className="new-album__image image"
-          loading="lazy"
-          src={logo}
-          alt="New Album"
-        />
+    <div className={"new-album"}>
+      <div className={"wrapper"}>
+        <img className={"image"} src={logo} alt={"New Album"} />
       </div>
-      <div className="new-album__wrapper">
+      <div className={"wrapper"}>
         <MusicServices />
-        <h2 className="new-album__title title">2020 Album</h2>
-        <p className="new-album__text text">
+        <h2 className={"title"}>2020 Album</h2>
+        <p className={"text"}>
           Our first album contains stories about who is Darwin's Cat, how Cat's
           Civilisation borns, a story about Cat's evolution process, long story
           about what Darwin's Cat saw when he returned to the cat's civilisation
@@ -26,11 +21,12 @@ export const NewAlbum = () => {
           cyber cats do and how they interact with Humans. Verbal terms of the
           tales are adapted to be understandable for Humans.
         </p>
-        <a className="new-album__button button" href="#music.html">
-          Read more
+        <a className={"read-more"} href={"#music.html"}>
+          Listen music
         </a>
-
-        <SocialNetworks />
+        <div className={"social-networks"}>
+          <SocialNetworks />
+        </div>
       </div>
     </div>
   );

4c2806d574af23c90ed9f61717840bdef4f2a050
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-06-14 16:41:39 +0300

    mobile audio player

--- src/album/SongControlLine.tsx
+++ src/album/SongControlLine.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SongControlLine.css";
 import { OrderLabel } from "./OrderLabel";
 
@@ -13,8 +13,30 @@ type Props = {
 };
 
 export const SongControlLine = (props: Props) => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  useEffect(() => {
+    const updateWidth = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+  const onRootClick = () => {
+    if (width >= 768) {
+      return undefined;
+    } else {
+      if (props.isCurrent && props.isPlaying) {
+        return props.handlePause;
+      } else {
+        return props.handlePlay;
+      }
+    }
+  };
+
   return (
-    <div className={`song-control-line ${props.isCurrent ? "selected" : ""}`}>
+    <div
+      className={`song-control-line ${props.isCurrent ? "selected" : ""}`}
+      onClick={onRootClick()}
+    >
       <OrderLabel
         order={props.order}
         isPlaying={props.isCurrent && props.isPlaying}
